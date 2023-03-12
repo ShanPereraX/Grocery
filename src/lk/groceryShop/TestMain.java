f77c1477aa9326e68c48b46541c8f17bae737222
ShanPereraX <perera.alc2000@gmail.com>
2023-03-12 22:06:29 +0530

    customer validation updated

---
+++ src/lk/groceryShop/TestMain.java
@@ -0,0 +1,15 @@
+package lk.groceryShop;
+
+import lk.groceryShop.dao.custom.impl.CustomerDaoImpl;
+import lk.groceryShop.dao.custom.impl.ItemDaoImpl;
+import lk.groceryShop.dao.custom.impl.OrderDaoImpl;
+import lk.groceryShop.entity.Customer;
+import lk.groceryShop.entity.Item;
+import lk.groceryShop.entity.Orders;
+import lk.groceryShop.util.FactoryConfiguration;
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
